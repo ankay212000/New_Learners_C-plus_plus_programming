8035a3afd05cbdfc1c327988c050eae9e086027d
ANKAY <52236029+ankay212000@users.noreply.github.com>
2020-09-30 23:29:57 +0530

    Create Problem4.cpp

---
+++ Problem4.cpp
@@ -0,0 +1,14 @@
+// explain every step by adding comments to every line 
+// And what this program is doing
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int a,b;
+   cin>>a>>b;
+   b=(a+b)-(a=b);
+   cout<<a<<" "<<b;
+   return 0;
+}
