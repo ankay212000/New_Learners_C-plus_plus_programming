38e4d5434002fd0cb0b4392ba6486f48485c1a7a
ANKAY <52236029+ankay212000@users.noreply.github.com>
2020-09-30 23:31:50 +0530

    Create Problem 5.cpp

---
+++ Problem 5.cpp
@@ -0,0 +1,12 @@
+// find the bug and correct it
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   string name="let's go";
+   char ask="where";
+   cout<<name+ask;
+   return 0;
+}
