a043c82c7498ff1a818f49a300bf086aa9940c1b
ANKAY <52236029+ankay212000@users.noreply.github.com>
2020-10-01 00:04:18 +0530

    Create Program 10.cpp

---
+++ Program 10.cpp
@@ -0,0 +1,14 @@
+// What does this program do?
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int arr[]={1,2,3,4};
+  int j=2;
+  for(int i=j;i<4;i++)
+  cout<<arr[i]<<" ";
+  for(int i=0;i<j;i++)
+  cout<<arr[i]<<" "; 
+  return 0;
+}
