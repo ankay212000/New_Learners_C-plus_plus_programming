163afa72bc92c3244a43ce69662bf6e3ae997ca8
GAURAV PATIL <67456816+GauravPatil8778@users.noreply.github.com>
2020-10-01 14:34:00 +0530

    Create Min_Max_without_comparison_operator.cpp

---
+++ Min_Max_without_comparison_operator.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+   int a,b;
+   cout<<"Enter two Numbers"<<endl;
+   cin>>a>>b;
+   
+   if(a^b)
+   {
+     cout<<"Not Same";
+   }
+   else
+   {
+     cout<<"Same";
+   }
+
+return 0;
+}
